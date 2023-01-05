35ad19eba1e3aed373e0fe8a84b53a365fe83ca5
Djaltsyn Do <diegonajurieta@gmail.com>
2023-01-05 11:38:49 -0300

    controladores

--- src/main/java/com/spring/ecommerce/ECommerceApplication.java
+++ src/main/java/com/spring/ecommerce/ECommerceApplication.java
@@ -2,13 +2,14 @@ package com.spring.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+
+@SpringBootApplication
 public class ECommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceApplication.class, args);
+	
 	}
 
 }
--- src/main/java/com/spring/ecommerce/model/Usuario.java
+++ src/main/java/com/spring/ecommerce/model/Usuario.java
@@ -1,7 +1,21 @@
 package com.spring.ecommerce.model;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    
     private String name;
     private String username;
     private String email;
@@ -9,6 +23,12 @@ public class Usuario {
     private String telefono;
     private String tipo;
     private String password;
+    
+    @OneToMany(mappedBy = "usuario")
+    private List<Producto> productos;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Orden> ordenes;
     
     public Usuario(){
 
@@ -24,6 +44,7 @@ public class Usuario {
         this.telefono = telefono;
         this.tipo = tipo;
         this.password = password;
+
     }
     public Integer getId() {
         return id;
@@ -74,10 +95,20 @@ public class Usuario {
         this.password = password;
     }
 
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    } 
+
     @Override
     public String toString() {
         return "Usuario [id=" + id + ", name=" + name + ", username=" + username + ", email=" + email + ", direccion="
                 + direccion + ", telefono=" + telefono + ", tipo=" + tipo + ", password=" + password + "]";
     }
+
+
     
 }
